368c930b72ecaad2cb393ca79e0bf7549b9d9e73
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-01-10 02:57:10 +0900

    fix tig

--- config/Makefile.d/var.d/tig.mk
+++ config/Makefile.d/var.d/tig.mk
@@ -1,6 +1,5 @@
 TIG_DIR = $(PLUGINS_GIT_DIR)/tig
 
-TIG_BUILD_COMMAND = $(BIN_DIR)/build/tig
 TIG_LAST_BUILD = $(TIG_DIR)/.tig-last-build
 TIG_REPO = git://github.com/jonas/tig.git
 TIG_VAR_MK = $(MAKEFILE_VAR_DIR)/tig.mk
