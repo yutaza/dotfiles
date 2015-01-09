9838bd92f596c9f09234f2b5c834da36893a6eec
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-01-09 18:03:31 +0900

    use php-build

--- config/Makefile.d/task.d/phpenv.mk
+++ config/Makefile.d/task.d/phpenv.mk
@@ -2,10 +2,13 @@ $(VAR_PHPENV_DIR):
 	git clone $(PHPENV_REPO) $@
 	$(VAR_PHPENV_DIR)/bin/phpenv-install.sh
 
+$(PHP_BUILD_DIR):
+	git clone $(PHP_BUILD_REPO) $@
+
 .PHONY: phpenv-distclean
 phpenv-distclean:
 	@rm -rf $(PHPENV_DIR)
 	@rm -rf $(VAR_PHPENV_DIR)
 
 .PHONY: phpenv-install
-phpenv-install: $(VAR_PHPENV_DIR)
+phpenv-install: $(VAR_PHPENV_DIR) $(PHP_BUILD_DIR)
--- config/Makefile.d/var.d/phpenv.mk
+++ config/Makefile.d/var.d/phpenv.mk
@@ -1,4 +1,6 @@
 PHPENV_DIR = ~/.phpenv
 VAR_PHPENV_DIR = $(VAR_DIR)/phpenv
-
 PHPENV_REPO = git@github.com:CHH/phpenv.git
+
+PHP_BUILD_DIR = $(PHPENV_DIR)/plugins/php-build
+PHP_BUILD_REPO = git@github.com:CHH/php-build.git
