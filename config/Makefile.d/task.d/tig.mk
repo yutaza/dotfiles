5be3e4e52a01083241889e998505b5034fd4adb3
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-01-11 03:30:47 +0900

    fix tig

--- config/Makefile.d/task.d/tig.mk
+++ config/Makefile.d/task.d/tig.mk
@@ -9,7 +9,7 @@ $(TIG_LAST_BUILD): $(TIG_DIR) $(TIG_VAR_MK)
 	make --directory=$(TIG_DIR) distclean
 	make --directory=$(TIG_DIR) prefix=$(TIG_DIR)
 	make --directory=$(TIG_DIR) install prefix=$(TIG_DIR)
-	$(XLN) $(TIG_DIR)/bin/tig $(BIN_DIR)/tig
+	$(XLN) $(TIG_VAR_BIN) $(TIG_BIN)
 	touch $@
 
 .PHONY: tig-build
@@ -18,11 +18,11 @@ tig-build: $(TIG_LAST_BUILD)
 .PHONY: tig-clean
 tig-clean:
 	$(RM) $(TIG_LAST_BUILD)
+	$(RM) $(TIG_BIN)
 
 .PHONY: tig-distclean
 tig-distclean:
 	$(RM) $(TIG_DIR)
-	$(RM) $(BIN_DIR)/tig
 
 .PHONY: tig-install
 tig-install: $(TIG_DIR)
--- config/Makefile.d/var.d/tig.mk
+++ config/Makefile.d/var.d/tig.mk
@@ -1,4 +1,6 @@
 TIG_DIR = $(PLUGINS_GIT_DIR)/tig
+TIG_BIN = $(BIN_DIR)/tig
+TIG_VAR_BIN = $(TIG_DIR)/bin/tig
 
 TIG_GIT_COMMAND = git -C $(TIG_DIR)
 TIG_LAST_BUILD = $(TIG_DIR)/.tig-last-build
