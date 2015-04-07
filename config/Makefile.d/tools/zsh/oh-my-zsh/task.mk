d7e48f1a8eeee8244d52e6cbfc1e339e80743dd8
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-04-07 23:34:28 +0900

    add oh-my-zsh

---
+++ config/Makefile.d/tools/zsh/oh-my-zsh/task.mk
@@ -0,0 +1,9 @@
+$(OH_MY_ZSH_DIR):
+	git clone $(OH_MY_ZSH_REPO) $@
+
+.PHONY: oh-my-zsh-distclean
+oh-my-zsh-distclean:
+	rm -rf $(OH_MY_ZSH_DIR)
+
+.PHONY: oh-my-zsh-install
+oh-my-zsh-install: $(OH_MY_ZSH_DIR)
---
+++ config/Makefile.d/tools/zsh/oh-my-zsh/var.mk
@@ -0,0 +1,5 @@
+OH_MY_ZSH_DIR = $(HOME)/.oh-my-zsh
+OH_MY_ZSH_REPO = git://github.com/robbyrussell/oh-my-zsh.git
+
+DISTCLEAN_TARGETS += oh-my-zsh-distclean
+INSTALL_TARGETS += oh-my-zsh-install
