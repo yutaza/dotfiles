de84c5e8a7f4e06d188d098e8573b3b5ea0139e7
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-04-08 00:05:46 +0900

    fix oh-my-zsh

--- config/Makefile.d/tools/zsh/oh-my-zsh/var.mk
+++ config/Makefile.d/tools/zsh/oh-my-zsh/var.mk
@@ -1,5 +1,5 @@
 OH_MY_ZSH_DIR = $(HOME)/.oh-my-zsh
 OH_MY_ZSH_REPO = git://github.com/robbyrussell/oh-my-zsh.git
 
+BUILD_TARGETS += oh-my-zsh-build
 DISTCLEAN_TARGETS += oh-my-zsh-distclean
-INSTALL_TARGETS += oh-my-zsh-install
