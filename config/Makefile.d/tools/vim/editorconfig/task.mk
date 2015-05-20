96bf344dd1badbab5e22d62ff3fc52ade2fc60f3
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-05-20 20:42:25 +0900

    fix editorconfig

--- config/Makefile.d/tools/vim/editorconfig/task.mk
+++ config/Makefile.d/tools/vim/editorconfig/task.mk
@@ -1,31 +1,31 @@
-$(EDITORCONFIG_DIR):
-	git clone $(EDITORCONFIG_REPO) $@
+$(VIM_EDITORCONFIG_DIR):
+	git clone $(VIM_EDITORCONFIG_REPO) $@
 
-$(EDITORCONFIG_AUTOLOAD_DIR): $(VIM_AUTOLOAD_DIR)
+$(VIM_EDITORCONFIG_AUTOLOAD_DIR): $(VIM_AUTOLOAD_DIR)
 	$(XLN) $@ $<
 
-$(EDITORCONFIG_PLUGIN_DIR): $(VIM_PLUGIN_DIR)
+$(VIM_EDITORCONFIG_PLUGIN_DIR): $(VIM_PLUGIN_DIR)
 	$(XLN) $@ $<
 
-$(EDITORCONFIG_LAST_BUILD): $(EDITORCONFIG_DIR) $(EDITORCONFIG_VAR_MK) $(EDITORCONFIG_AUTOLOAD_DIR) $(EDITORCONFIG_PLUGIN_DIR)
+$(VIM_EDITORCONFIG_LAST_BUILD): $(VIM_EDITORCONFIG_DIR) $(VIM_EDITORCONFIG_VAR_MK) $(VIM_EDITORCONFIG_AUTOLOAD_DIR) $(VIM_EDITORCONFIG_PLUGIN_DIR)
 	rm -rf $@
-	$(EDITORCONFIG_GIT_COMMAND) fetch -p
-	$(EDITORCONFIG_GIT_COMMAND) reset --hard $(EDITORCONFIG_GIT_TAG)
+	$(VIM_EDITORCONFIG_GIT_COMMAND) fetch -p
+	$(VIM_EDITORCONFIG_GIT_COMMAND) reset --hard $(VIM_EDITORCONFIG_GIT_TAG)
 	touch $@
 
-.PHONY: editorconfig-build
-editorconfig-build: $(EDITORCONFIG_LAST_BUILD)
+.PHONY: vim_editorconfig-build
+vim_editorconfig-build: $(VIM_EDITORCONFIG_LAST_BUILD)
 
-.PHONY: editorconfig-clean
-editorconfig-clean:
-	rm -rf $(EDITORCONFIG_LAST_BUILD)
+.PHONY: vim_editorconfig-clean
+vim_editorconfig-clean:
+	rm -rf $(VIM_EDITORCONFIG_LAST_BUILD)
 
-.PHONY: editorconfig-distclean
-editorconfig-distclean:
-	rm -rf $(EDITORCONFIG_DIR)
+.PHONY: vim_editorconfig-distclean
+vim_editorconfig-distclean:
+	rm -rf $(VIM_EDITORCONFIG_DIR)
 
-.PHONY: editorconfig-install
-editorconfig-install: $(EDITORCONFIG_DIR)
+.PHONY: vim_editorconfig-install
+vim_editorconfig-install: $(VIM_EDITORCONFIG_DIR)
 
-.PHONY: editorconfig-update
-editorconfig-update: editorconfig-clean editorconfig-build
+.PHONY: vim_editorconfig-update
+vim_editorconfig-update: vim_editorconfig-clean vim_editorconfig-build
--- config/Makefile.d/tools/vim/editorconfig/var.mk
+++ config/Makefile.d/tools/vim/editorconfig/var.mk
@@ -1,25 +1,25 @@
-EDITORCONFIG_DIR = $(VAR_DIR)/plugins/vim/editorconfig-vim
+VIM_EDITORCONFIG_DIR = $(VAR_DIR)/plugins/vim/editorconfig-vim
 
 #FIXME
-EDITORCONFIG_AUTOLOAD_DIR = $(EDITORCONFIG_DIR)/autoload
-EDITORCONFIG_PLUGIN_DIR = $(EDITORCONFIG_DIR)/plugin
+VIM_EDITORCONFIG_AUTOLOAD_DIR = $(VIM_EDITORCONFIG_DIR)/autoload
+VIM_EDITORCONFIG_PLUGIN_DIR = $(VIM_EDITORCONFIG_DIR)/plugin
 
-EDITORCONFIG_GIT_COMMAND = git --git-dir=$(EDITORCONFIG_DIR)/.git --work-tree=$(EDITORCONFIG_DIR)
-EDITORCONFIG_LAST_BUILD = $(EDITORCONFIG_DIR)/.editorconfig-last-build
-EDITORCONFIG_REPO = git://github.com/editorconfig/editorconfig-vim.git
-EDITORCONFIG_TAG_PREFIX = v
-EDITORCONFIG_TAG_SUFFIX =
-EDITORCONFIG_VAR_MK = $(CONFIG_DIR)/Makefile.d/tools/vim/editorconfig/var.mk
-EDITORCONFIG_VERSION = latest
+VIM_EDITORCONFIG_GIT_COMMAND = git --git-dir=$(VIM_EDITORCONFIG_DIR)/.git --work-tree=$(VIM_EDITORCONFIG_DIR)
+VIM_EDITORCONFIG_LAST_BUILD = $(VIM_EDITORCONFIG_DIR)/.editorconfig-last-build
+VIM_EDITORCONFIG_REPO = git://github.com/editorconfig/editorconfig-vim.git
+VIM_EDITORCONFIG_TAG_PREFIX = v
+VIM_EDITORCONFIG_TAG_SUFFIX =
+VIM_EDITORCONFIG_VAR_MK = $(CONFIG_DIR)/Makefile.d/tools/vim/editorconfig/var.mk
+VIM_EDITORCONFIG_VERSION = latest
 
-ifeq ($(EDITORCONFIG_VERSION), latest)
-    EDITORCONFIG_GIT_TAG = origin/HEAD
+ifeq ($(VIM_EDITORCONFIG_VERSION), latest)
+    VIM_EDITORCONFIG_GIT_TAG = origin/HEAD
 else
-    EDITORCONFIG_GIT_TAG = $(EDITORCONFIG_TAG_PREFIX)$(EDITORCONFIG_VERSION)$(EDITORCONFIG_TAG_SUFFIX)
+    VIM_EDITORCONFIG_GIT_TAG = $(VIM_EDITORCONFIG_TAG_PREFIX)$(VIM_EDITORCONFIG_VERSION)$(VIM_EDITORCONFIG_TAG_SUFFIX)
 endif
 
-BUILD_TARGETS += editorconfig-build
-CLEAN_TARGETS += editorconfig-clean
-DISTCLEAN_TARGETS += editorconfig-distclean
-INSTALL_TARGETS += editorconfig-install
-UPDATE_TARGETS += editorconfig-update
+BUILD_TARGETS += vim_editorconfig-build
+CLEAN_TARGETS += vim_editorconfig-clean
+DISTCLEAN_TARGETS += vim_editorconfig-distclean
+INSTALL_TARGETS += vim_editorconfig-install
+UPDATE_TARGETS += vim_editorconfig-update
