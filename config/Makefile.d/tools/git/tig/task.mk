163f84cbcf424fa9e6c138550d6e779b50dcf9e9
yutaka ozaki <y_ozaki@gochipon.co.jp>
2015-06-19 00:49:19 +0900

    update tig

--- config/Makefile.d/tools/git/tig/task.mk
+++ config/Makefile.d/tools/git/tig/task.mk
@@ -1,21 +1,8 @@
 $(TIG_DIR):
 	git clone $(TIG_REPO) $@
-
-$(TIG_LAST_BUILD): $(TIG_DIR) $(TIG_VAR_MK)
-	rm -rf $@
-	$(TIG_GIT_COMMAND) fetch -p
-	$(TIG_GIT_COMMAND) reset --hard $(TIG_GIT_TAG)
 	make --directory=$(TIG_DIR) distclean
 	make --directory=$(TIG_DIR) prefix=$(TIG_DIR)
 	make --directory=$(TIG_DIR) install prefix=$(TIG_DIR)
-	touch $@
-
-.PHONY: tig-build
-tig-build: $(TIG_LAST_BUILD)
-
-.PHONY: tig-clean
-tig-clean:
-	rm -rf $(TIG_LAST_BUILD)
 
 .PHONY: tig-distclean
 tig-distclean:
@@ -25,4 +12,8 @@ tig-distclean:
 tig-install: $(TIG_DIR)
 
 .PHONY: tig-update
-tig-update: tig-clean tig-build
+tig-update: $(TIG_DIR)
+	cd $< && git fetch -p && git pull
+	make --directory=$(TIG_DIR) distclean
+	make --directory=$(TIG_DIR) prefix=$(TIG_DIR)
+	make --directory=$(TIG_DIR) install prefix=$(TIG_DIR)
