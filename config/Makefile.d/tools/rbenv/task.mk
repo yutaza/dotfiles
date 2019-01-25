3eff5e1a7bf478d56d1077f7a7ec78553506045e
yutaka ozaki <y_ozaki@gochipon.co.jp>
2019-01-25 10:48:01 +0900

    Add ruby-build

--- config/Makefile.d/tools/rbenv/task.mk
+++ config/Makefile.d/tools/rbenv/task.mk
@@ -1,13 +1,17 @@
 $(RBENV_DIR):
 	git clone $(RBENV_REPO) $@
 
+$(RBENV_BUILD_DIR):
+	git clone $(RBENV_BUILD_REPO) $@
+
 .PHONY: rbenv-distclean
 rbenv-distclean:
 	rm -rf $(RBENV_DIR)
 
 .PHONY: rbenv-install
-rbenv-install: $(RBENV_DIR)
+rbenv-install: $(RBENV_DIR) $(RBENV_BUILD_DIR)
 
 .PHONY: rbenv-update
-rbenv-update: $(RBENV_DIR)
-	cd $< && git fetch -p && git pull
+rbenv-update: $(RBENV_DIR) $(RBENV_BUILD_DIR)
+	cd $(RBENV_DIR) && git fetch -p && git pull
+	cd $(RBENV_BUILD_DIR) && git fetch -p && git pull
--- config/Makefile.d/tools/rbenv/var.mk
+++ config/Makefile.d/tools/rbenv/var.mk
@@ -1,5 +1,7 @@
 RBENV_DIR = $(HOME)/.rbenv
 RBENV_REPO = git://github.com/rbenv/rbenv
+RBENV_BUILD_DIR = $(RBENV_DIR)/plugins/ruby-build
+RBENV_BUILD_REPO = git://github.com/rbenv/ruby-build
 
 DISTCLEAN_TARGETS += rbenv-distclean
 INSTALL_TARGETS += rbenv-install
