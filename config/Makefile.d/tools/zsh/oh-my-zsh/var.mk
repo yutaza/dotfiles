784c573adb09c6724d4d1be26279702ccae9404f
yutaka ozaki <y_ozaki@gochipon.co.jp>
2023-09-29 12:13:35 +0900

    chore: update config

--- config/Makefile.d/tools/zsh/oh-my-zsh/var.mk
+++ config/Makefile.d/tools/zsh/oh-my-zsh/var.mk
@@ -1,5 +1,5 @@
 OH_MY_ZSH_DIR = $(HOME)/.oh-my-zsh
-OH_MY_ZSH_REPO = git://github.com/robbyrussell/oh-my-zsh.git
+OH_MY_ZSH_REPO = git@github.com:ohmyzsh/ohmyzsh.git
 
 DISTCLEAN_TARGETS += oh-my-zsh-distclean
 INSTALL_TARGETS += oh-my-zsh-install
