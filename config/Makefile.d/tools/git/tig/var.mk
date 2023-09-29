f1f5d0a69c32b4fb1e5cc22a8b61cc3e6372e859
yutaka ozaki <y_ozaki@gochipon.co.jp>
2023-09-29 13:09:30 +0900

    chore: refactoring

--- config/Makefile.d/tools/git/tig/var.mk
+++
@@ -1,6 +0,0 @@
-GIT_TIG_DIR = $(VAR_DIR)/plugins/git/tig
-GIT_TIG_REPO = git://github.com/jonas/tig.git
-
-DISTCLEAN_TARGETS += git-tig-distclean
-INSTALL_TARGETS += git-tig-install
-UPDATE_TARGETS += git-tig-update
--- config/Makefile.d/tools/goenv/var.mk
+++
@@ -1,8 +0,0 @@
-GOENV_DIR = $(HOME)/.goenv
-GOENV_PACKAGES_DIR = $(HOME)/.goenv/packages
-GOENV_REPO = git://github.com/syndbg/goenv
-GOENV_GO_VERSION = 1.11.2
-
-DISTCLEAN_TARGETS += goenv-distclean
-INSTALL_TARGETS += goenv-install
-UPDATE_TARGETS += goenv-update
